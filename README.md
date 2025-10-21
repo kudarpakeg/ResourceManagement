7c44de2dc6ccbc5b88cf3e3c7f7ceb8631d32c41
kudarpakeg <kudarpakeg@gmail.com>
2025-10-21 11:46:47 +0530

    Initial push

---
+++ README.md
@@ -0,0 +1,2 @@
+# Resource Management System (RMS)
+Starter repo scaffold for .NET 8 + Angular + Azure architecture.
---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,2 @@
+import { Component } from '@angular/core';
+@Component({ selector: 'app-root', template: `<h1>RMS Starter</h1>` }) export class AppComponent {}
